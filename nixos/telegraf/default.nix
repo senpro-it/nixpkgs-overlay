e5ce232960ca6810656201eeaa37d93dd960f8cb
senpro-faesslerl <110035719+senpro-faesslerl@users.noreply.github.com>
2023-02-15 18:42:20 +0100

    add: Sophos XG service monitoring (telegraf)

--- nixos/telegraf/default.nix
+++ nixos/telegraf/default.nix
@@ -932,12 +932,164 @@ let cfg = config.senpro; in {
                   is_tag = true;
                 }
                 {
+                  name = "licenseBaseFWStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosBaseFWLicRegStatus.0";
+                }
+                {
+                  name = "licenseBaseFWExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosBaseFWLicExpiryDate.0";
+                }
+                {
+                  name = "licenseNetProtectionStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosNetProtectionLicRegStatus.0";
+                }
+                {
+                  name = "licenseNetProtectionExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosNetProtectionLicExpiryDate.0";
+                }
+                {
+                  name = "licenseWebProtectionStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosWebProtectionLicRegStatus.0";
+                }
+                {
+                  name = "licenseWebProtectionExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosWebProtectionLicExpiryDate.0";
+                }
+                {
+                  name = "licenseMailProtectionStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosMailProtectionLicRegStatus.0";
+                }
+                {
+                  name = "licenseMailProtectionExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosMailProtectionLicExpiryDate.0";
+                }
+                {
+                  name = "licenseWebServerProtectionStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosWebServerProtectionLicRegStatus.0";
+                }
+                {
+                  name = "licenseWebServerProtectionExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosWebServerProtectionLicExpiryDate.0";
+                }
+                {
+                  name = "licenseSandstromProtectionStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosSandstromLicRegStatus.0";
+                }
+                {
+                  name = "licenseSandstromProtectionExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosSandstromLicExpiryDate.0";
+                }
+                {
+                  name = "licenseEnhancedSupportStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosEnhancedSupportLicRegStatus.0";
+                }
+                {
+                  name = "licenseEnhancedSupportExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosEnhancedSupportLicExpiryDate.0";
+                }
+                {
+                  name = "licenseEnhancedSupportPlusStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosEnhancedPlusLicRegStatus.0";
+                }
+                {
+                  name = "licenseEnhancedSupportPlusExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosEnhancedPlusLicExpiryDate.0";
+                }
+                {
+                  name = "licenseCentralOrchestrationStatus";
+                  oid = "SFOS-FIREWALL-MIB::sfosCentralOrchestrationLicRegStatus.0";
+                }
+                {
+                  name = "licenseCentralOrchestrationExpiry";
+                  oid = "SFOS-FIREWALL-MIB::sfosCentralOrchestrationLicExpiryDate.0";
+                }
+                {
                   name = "memoryCapacity";
                   oid = "SFOS-FIREWALL-MIB::sfosMemoryCapacity.0";
                 }
                 {
                   name = "memoryPercentUsage";
                   oid = "SFOS-FIREWALL-MIB::sfosMemoryPercentUsage.0";
+                }
+                {
+                  name = "serviceApache";
+                  oid = "SFOS-FIREWALL-MIB::sfosApacheService.0";
+                }
+                {
+                  name = "serviceAS";
+                  oid = "SFOS-FIREWALL-MIB::sfosASService.0";
+                }
+                {
+                  name = "serviceAV";
+                  oid = "SFOS-FIREWALL-MIB::sfosAVService.0";
+                }
+                {
+                  name = "serviceDatabase";
+                  oid = "SFOS-FIREWALL-MIB::sfosDatabaseservice.0";
+                }
+                {
+                  name = "serviceDGD";
+                  oid = "SFOS-FIREWALL-MIB::sfosDgdService.0";
+                }
+                {
+                  name = "serviceDNS";
+                  oid = "SFOS-FIREWALL-MIB::sfosDNSService.0";
+                }
+                {
+                  name = "serviceDRouting";
+                  oid = "SFOS-FIREWALL-MIB::sfosDroutingService.0";
+                }
+                {
+                  name = "serviceFTP";
+                  oid = "SFOS-FIREWALL-MIB::sfosFtpService.0";
+                }
+                {
+                  name = "serviceGarner";
+                  oid = "SFOS-FIREWALL-MIB::sfosGarnerService.0";
+                }
+                {
+                  name = "serviceHA";
+                  oid = "SFOS-FIREWALL-MIB::sfosHAService.0";
+                }
+                {
+                  name = "serviceHTTP";
+                  oid = "SFOS-FIREWALL-MIB::sfosHttpService.0";
+                }
+                {
+                  name = "serviceIMAP4";
+                  oid = "SFOS-FIREWALL-MIB::sfosImap4Service.0";
+                }
+                {
+                  name = "serviceIP";
+                  oid = "SFOS-FIREWALL-MIB::sfosIPService.0";
+                }
+                {
+                  name = "serviceIPSec";
+                  oid = "SFOS-FIREWALL-MIB::sfosIPSecVpnService.0";
+                }
+                {
+                  name = "serviceNetwork";
+                  oid = "SFOS-FIREWALL-MIB::sfosNetworkService.0";
+                }
+                {
+                  name = "serviceNTP";
+                  oid = "SFOS-FIREWALL-MIB::sfosNtpService.0";
+                }
+                {
+                  name = "servicePOP3";
+                  oid = "SFOS-FIREWALL-MIB::sfosPoP3Service.0";
+                }
+                {
+                  name = "serviceSSHd";
+                  oid = "SFOS-FIREWALL-MIB::sfosSSHdService.0";
+                }
+                {
+                  name = "serviceSSLVPN";
+                  oid = "SFOS-FIREWALL-MIB::sfosSSLVpnService.0";
+                }
+                {
+                  name = "serviceTomcat";
+                  oid = "SFOS-FIREWALL-MIB::sfosTomcatService.0";
                 }
                 {
                   name = "swapCapacity";
