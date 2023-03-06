e5ca12af282c791769c41ae188c885208cc612ca
senpro-faesslerl <110035719+senpro-faesslerl@users.noreply.github.com>
2023-03-06 18:29:55 +0100

    fix: pin alertmanager, grafana and influxdb to version

--- nixos/oci-containers/grafana/default.nix
+++ nixos/oci-containers/grafana/default.nix
@@ -147,95 +147,93 @@ let cfg = config.senpro; in {
     };
   };
 
-  config = {
-    virtualisation.oci-containers.containers = lib.mkIf (cfg.oci-containers != {}) (lib.mkMerge [
-      (lib.mkIf cfg.oci-containers.grafana.enable {
-        grafana = {
-          image = "docker.io/grafana/grafana:latest";
-          autoStart = true;
-          dependsOn = [
-            "grafana-influxdb"
-          ];
-          extraOptions = [
-            "--net=proxy"
-            "--label=traefik.enable=true"
-            "--label=traefik.http.routers.grafana.tls=true"
-            "--label=traefik.http.routers.grafana.entrypoints=https2-tcp"
-            "--label=traefik.http.routers.grafana.service=grafana"
-            "--label=traefik.http.routers.grafana.rule=Host(`${cfg.oci-containers.grafana.rootURL}`)"
-            "--label=traefik.http.services.grafana.loadBalancer.server.port=3000"
-            "--label=traefik.http.middlewares.grafanaAuthProxy.basicAuth.users=${cfg.oci-containers.grafana.authProxy.noc.username}:${cfg.oci-containers.grafana.authProxy.noc.password}"
-          ];
-          environment = {
-            GF_USERS_ALLOW_SIGN_UP = "false";
-            GF_USERS_AUTO_ASSIGN_ORG = "true";
-            GF_AUTH_DISABLE_LOGIN_FORM = "true";
-            GF_AUTH_GENERIC_OAUTH_ENABLED = "true";
-            GF_AUTH_GENERIC_OAUTH_NAME = "OAuth2";
-            GF_AUTH_GENERIC_OAUTH_ALLOW_SIGN_UP = "true";
-            GF_AUTH_GENERIC_OAUTH_CLIENT_ID = "grafana";
-            GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET = "${cfg.oci-containers.grafana.keycloak.client.secret}";
-            GF_AUTH_GENERIC_OAUTH_SCOPES = "openid profile email";
-            GF_AUTH_GENERIC_OAUTH_AUTH_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/auth";
-            GF_AUTH_GENERIC_OAUTH_TOKEN_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/token";
-            GF_AUTH_GENERIC_OAUTH_API_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/userinfo";
-            GF_AUTH_GENERIC_OAUTH_TLS_SKIP_VERIFY_INSECURE = "true";
-            GF_AUTH_OAUTH_SKIP_ORG_ROLE_UPDATE_SYNC = "true";
-            GF_SERVER_ROOT_URL = "https://${cfg.oci-containers.grafana.rootURL}";
-            GF_PANELS_DISABLE_SANITIZE_HTML = "true";
-            GF_FEATURE_TOGGLES_ENABLE = "internationalization newPanelChromeUI publicDashboards";
-            GF_SMTP_ENABLED = "true";
-            GF_SMTP_HOST = "${cfg.oci-containers.grafana.smtp.host}:${toString cfg.oci-containers.grafana.smtp.port}";
-            GF_SMTP_USER = "${cfg.oci-containers.grafana.smtp.username}";
-            GF_SMTP_PASSWORD = "${cfg.oci-containers.grafana.smtp.password}";
-            GF_SMTP_FROM_NAME = "${cfg.oci-containers.grafana.smtp.displayName}";
-            GF_SMTP_FROM_ADDRESS = "${cfg.oci-containers.grafana.smtp.from}";
-            GF_AUTH_PROXY_ENABLED = "true";
-            GF_AUTH_PROXY_HEADER_NAME = "X-WEBAUTH-USER";
-            GF_AUTH_PROXY_HEADER_PROPERTY = "username";
-            GF_AUTH_PROXY_AUTO_SIGN_UP = "true";
-            GF_AUTH_PROXY_WHITELIST = "${cfg.oci-containers.grafana.authProxy.whitelist}";
-          };
-          volumes = [
-            "grafana:/etc/grafana/provisioning"
-            "grafana-data:/var/lib/grafana"
-          ];
+  config = mkIf cfg.oci-containers.grafana.enable {
+    virtualisation.oci-containers.containers = {
+      grafana = {
+        image = "docker.io/grafana/grafana:9.4.3";
+        autoStart = true;
+        dependsOn = [
+          "grafana-influxdb"
+        ];
+        extraOptions = [
+          "--net=proxy"
+          "--label=traefik.enable=true"
+          "--label=traefik.http.routers.grafana.tls=true"
+          "--label=traefik.http.routers.grafana.entrypoints=https"
+          "--label=traefik.http.routers.grafana.service=grafana"
+          "--label=traefik.http.routers.grafana.rule=Host(`${cfg.oci-containers.grafana.rootURL}`)"
+          "--label=traefik.http.services.grafana.loadBalancer.server.port=3000"
+          "--label=traefik.http.middlewares.grafanaAuthProxy.basicAuth.users=${cfg.oci-containers.grafana.authProxy.noc.username}:${cfg.oci-containers.grafana.authProxy.noc.password}"
+        ];
+        environment = {
+          GF_USERS_ALLOW_SIGN_UP = "false";
+          GF_USERS_AUTO_ASSIGN_ORG = "true";
+          GF_AUTH_DISABLE_LOGIN_FORM = "true";
+          GF_AUTH_GENERIC_OAUTH_ENABLED = "true";
+          GF_AUTH_GENERIC_OAUTH_NAME = "OAuth2";
+          GF_AUTH_GENERIC_OAUTH_ALLOW_SIGN_UP = "true";
+          GF_AUTH_GENERIC_OAUTH_CLIENT_ID = "grafana";
+          GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET = "${cfg.oci-containers.grafana.keycloak.client.secret}";
+          GF_AUTH_GENERIC_OAUTH_SCOPES = "openid profile email";
+          GF_AUTH_GENERIC_OAUTH_AUTH_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/auth";
+          GF_AUTH_GENERIC_OAUTH_TOKEN_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/token";
+          GF_AUTH_GENERIC_OAUTH_API_URL = "https://${cfg.oci-containers.grafana.keycloak.provider}/realms/${cfg.oci-containers.grafana.keycloak.realm}/protocol/openid-connect/userinfo";
+          GF_AUTH_GENERIC_OAUTH_TLS_SKIP_VERIFY_INSECURE = "true";
+          GF_AUTH_OAUTH_SKIP_ORG_ROLE_UPDATE_SYNC = "true";
+          GF_SERVER_ROOT_URL = "https://${cfg.oci-containers.grafana.rootURL}";
+          GF_PANELS_DISABLE_SANITIZE_HTML = "true";
+          GF_FEATURE_TOGGLES_ENABLE = "internationalization newPanelChromeUI publicDashboards";
+          GF_SMTP_ENABLED = "true";
+          GF_SMTP_HOST = "${cfg.oci-containers.grafana.smtp.host}:${toString cfg.oci-containers.grafana.smtp.port}";
+          GF_SMTP_USER = "${cfg.oci-containers.grafana.smtp.username}";
+          GF_SMTP_PASSWORD = "${cfg.oci-containers.grafana.smtp.password}";
+          GF_SMTP_FROM_NAME = "${cfg.oci-containers.grafana.smtp.displayName}";
+          GF_SMTP_FROM_ADDRESS = "${cfg.oci-containers.grafana.smtp.from}";
+          GF_AUTH_PROXY_ENABLED = "true";
+          GF_AUTH_PROXY_HEADER_NAME = "X-WEBAUTH-USER";
+          GF_AUTH_PROXY_HEADER_PROPERTY = "username";
+          GF_AUTH_PROXY_AUTO_SIGN_UP = "true";
+          GF_AUTH_PROXY_WHITELIST = "${cfg.oci-containers.grafana.authProxy.whitelist}";
         };
-        grafana-alertmanager = {
-          image = "ghcr.io/senpro-it/alertmanager:main";
-          autoStart = true;
-          extraOptions = [
-            "--net=proxy"
-            "--label=traefik.enable=true"
-            "--label=traefik.http.routers.grafana-alertmanager.tls=true"
-            "--label=traefik.http.routers.grafana-alertmanager.entrypoints=https2-tcp"
-            "--label=traefik.http.routers.grafana-alertmanager.service=grafana-alertmanager"
-            "--label=traefik.http.routers.grafana-alertmanager.rule=Host(`${cfg.oci-containers.grafana.alertmanager.publicURL}`)"
-            "--label=traefik.http.services.grafana-alertmanager.loadBalancer.server.port=9093"
-          ];
-          volumes = [
-            "grafana-alertmanager-data:/alertmanager"
-          ];
-        };
-        grafana-influxdb = {
-          image = "docker.io/library/influxdb:latest";
-          autoStart = true;
-          extraOptions = [
-            "--net=proxy"
-            "--label=traefik.enable=true"
-            "--label=traefik.http.routers.grafana-influxdb.tls=true"
-            "--label=traefik.http.routers.grafana-influxdb.entrypoints=https2-tcp"
-            "--label=traefik.http.routers.grafana-influxdb.service=grafana-influxdb"
-            "--label=traefik.http.routers.grafana-influxdb.rule=Host(`${cfg.oci-containers.grafana.influxdb.publicURL}`)"
-            "--label=traefik.http.services.grafana-influxdb.loadBalancer.server.port=8086"
-          ];
-          volumes = [
-            "grafana-influxdb:/etc/influxdb2"
-            "grafana-influxdb-data:/var/lib/influxdb2"
-          ];
-        };
-      })
-    ]);
+        volumes = [
+          "grafana:/etc/grafana/provisioning"
+          "grafana-data:/var/lib/grafana"
+        ];
+      };
+      grafana-alertmanager = {
+        image = "ghcr.io/senpro-it/alertmanager:main";
+        autoStart = true;
+        extraOptions = [
+          "--net=proxy"
+          "--label=traefik.enable=true"
+          "--label=traefik.http.routers.grafana-alertmanager.tls=true"
+          "--label=traefik.http.routers.grafana-alertmanager.entrypoints=https"
+          "--label=traefik.http.routers.grafana-alertmanager.service=grafana-alertmanager"
+          "--label=traefik.http.routers.grafana-alertmanager.rule=Host(`${cfg.oci-containers.grafana.alertmanager.publicURL}`)"
+          "--label=traefik.http.services.grafana-alertmanager.loadBalancer.server.port=9093"
+        ];
+        volumes = [
+          "grafana-alertmanager-data:/alertmanager"
+        ];
+      };
+      grafana-influxdb = {
+        image = "docker.io/library/influxdb:2.6";
+        autoStart = true;
+        extraOptions = [
+          "--net=proxy"
+          "--label=traefik.enable=true"
+          "--label=traefik.http.routers.grafana-influxdb.tls=true"
+          "--label=traefik.http.routers.grafana-influxdb.entrypoints=https"
+          "--label=traefik.http.routers.grafana-influxdb.service=grafana-influxdb"
+          "--label=traefik.http.routers.grafana-influxdb.rule=Host(`${cfg.oci-containers.grafana.influxdb.publicURL}`)"
+          "--label=traefik.http.services.grafana-influxdb.loadBalancer.server.port=8086"
+        ];
+        volumes = [
+          "grafana-influxdb:/etc/influxdb2"
+          "grafana-influxdb-data:/var/lib/influxdb2"
+        ];
+      };
+    };
   };
 
 }
