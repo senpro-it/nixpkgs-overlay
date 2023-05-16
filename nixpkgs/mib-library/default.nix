1578417f85f204273b8d7eafb506393fe408ca63
senpro-faesslerl <110035719+senpro-faesslerl@users.noreply.github.com>
2023-05-16 10:13:39 +0200

    upd: mib-library -> 2023-05-17.005

--- nixpkgs/mib-library/default.nix
+++ nixpkgs/mib-library/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "mib-library";
-  version = "2023-05-17.004";
+  version = "2023-05-17.005";
 
   src = fetchFromGitHub {
     owner = "senpro-it";
     repo = "mibs";
-    rev = "ba9faf8c972540b9d5121376947d55cf63cc81a4";
-    sha256 = "7PAOgYac0jt2Z8f9IT/fthgaL5ohWiRbCT0luZAdk7w=";
+    rev = "68909c98bef4441c88bf446f0b226bc7e39647b8";
+    sha256 = "YyAGlquy3ZEjzZTfImX6YrRXhwyksBPKyTSYbiSEFkA=";
   };
 
   installPhase = ''
