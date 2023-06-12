1a28f8d772195f9b1de8400735f026608cde3bf5
senpro-faesslerl <110035719+senpro-faesslerl@users.noreply.github.com>
2023-06-12 09:27:36 +0200

    fix: reworked SOPHOS SG (UTM) monitoring (nixos/telegraf)

--- nixos/monitoring/default.nix
+++ nixos/monitoring/default.nix
@@ -840,12 +840,25 @@ in {
                 { name = "description"; oid = "SNMPv2-MIB::sysDescr.0"; }
                 { name = "host"; oid = "SNMPv2-MIB::sysName.0"; is_tag = true; }
                 { name = "uptime"; oid = "SNMPv2-MIB::sysUpTime.0"; }
+                { name = "memAvailReal"; oid = "UCD-SNMP-MIB::memAvailReal.0"; }
+                { name = "memTotalReal"; oid = "UCD-SNMP-MIB::memTotalReal.0"; }
+                { name = "memTotalFree"; oid = "UCD-SNMP-MIB::memTotalFree.0"; }
+                { name = "memBuffer"; oid = "UCD-SNMP-MIB::memBuffer.0"; }
+                { name = "memCached"; oid = "UCD-SNMP-MIB::memCached.0"; }
+                { name = "memAvailSwap"; oid = "UCD-SNMP-MIB::memAvailSwap.0"; }
+                { name = "memTotalSwap"; oid = "UCD-SNMP-MIB::memTotalSwap.0"; }
               ];
               table = [
-                { name = "sophos.sg.cpu"; oid = "HOST-RESOURCES-MIB::hrProcessorTable"; index_as_tag = true; inherit_tags = [ "host" ]; }
-                { name = "sophos.sg.interface"; oid = "IF-MIB::ifTable"; inherit_tags = [ "host" ]; }
-                { name = "sophos.sg.ipAddresses"; oid = "IP-MIB::ipAddrTable"; inherit_tags = [ "host" ]; }
-                { name = "sophos.sg.storage"; oid = "HOST-RESOURCES-MIB::hrStorageTable"; inherit_tags = [ "host" ]; }
+                { name = "sophos.sg.dskTable"; oid = "UCD-SNMP-MIB::dskTable"; index_as_tag = true; inherit_tags = [ "host" ]; field = [
+                  { oid = "UCD-SNMP-MIB::dskDevice"; is_tag = true; }
+                ]; }
+                { name = "sophos.sg.ifTable"; oid = "IF-MIB::ifTable"; index_as_tag = true; inherit_tags = [ "host" ]; field = [
+                  { oid = "IF-MIB::ifDescr"; is_tag = true; }
+                ]; }
+                { name = "sophos.sg.ifXTable"; oid = "IF-MIB::ifXTable"; index_as_tag = true; inherit_tags = [ "host" ]; field = [
+                  { oid = "IF-MIB::ifName"; is_tag = true; }
+                ]; }
+                { name = "sophos.sg.ipAddrTable"; oid = "IP-MIB::ipAddrTable"; inherit_tags = [ "host" ]; }
               ];
             })
             (lib.mkIf cfg.monitoring.telegraf.inputs.snmp.vendors.sophos.xg.endpoints.self.enable {
@@ -1348,7 +1361,7 @@ in {
     };
 
     systemd.services = {
-      docker-svci-provisioner = {
+      docker-svci-provisioner = lib.mkIf cfg.monitoring.svci.enable {
         enable = true;
         description = "Provisioner for SVCi docker container.";
         requiredBy = [ "docker-svci.service" ];
