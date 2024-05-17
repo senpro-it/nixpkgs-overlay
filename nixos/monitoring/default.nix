5cf544d43f6b4412b7abe13a311fcfdd74beccc2
senpro-ziegerl <110035332+senpro-ziegerl@users.noreply.github.com>
2024-05-17 13:03:10 +0200

    Update default.nix

--- nixos/monitoring/default.nix
+++ nixos/monitoring/default.nix
@@ -1623,6 +1623,8 @@ in {
                 ]; }
                 { name = "sophos.xg.vpnIpsecTable"; oid = "SFOS-FIREWALL-MIB::sfosIPSecVpnTunnelTable"; index_as_tag = true; inherit_tags = [ "host" ]; field = [
                   { oid = "SFOS-FIREWALL-MIB::sfosIPSecVpnConnName"; is_tag = true; }
+                  { oid = "SFOS-FIREWALL-MIB::sfosIPSecVpnConnType"; is_tag = true; }
+                  { oid = "SFOS-FIREWALL-MIB::sfosIPSecVpnActivated"; is_tag = true; }
                 ]; }
                 { name = "sophos.xg.ipAddrTable"; oid = "IP-MIB::ipAddrTable"; inherit_tags = [ "host" ]; }
               ];
