510e305c55442250385035b773120161922d4a48
senpro-ziegerl <110035332+senpro-ziegerl@users.noreply.github.com>
2025-04-11 13:35:10 +0200

    Updated metrics for APC UPS

--- nixos/monitoring/default.nix
+++ nixos/monitoring/default.nix
@@ -1569,30 +1569,37 @@ in {
               priv_password = "${cfg.monitoring.telegraf.inputs.snmp.vendors.schneiderElectric.apc.credentials.privacy.password}";
               retries = 5;
               field = [
+                { name = "serialNumber"; oid = "PowerNet-MIB::upsAdvIdentSerialNumber.0"; }
+                { name = "firmwareRevision"; oid = "PowerNet-MIB::upsAdvIdentFirmwareRevision.0"; }
+                { name = "host"; oid = "PowerNet-MIB::upsBasicIdentName.0"; is_tag = true; }
+                { name = "model"; oid = "PowerNet-MIB::upsBasicIdentModel.0"; }
+                { name = "manufacturingDate"; oid = "PowerNet-MIB::upsAdvIdentDateOfManufacture.0"; }
+                { name = "upsSku"; oid = "PowerNet-MIB::upsAdvIdentSkuNumber.0"; }
+                { name = "batteryInternalSku"; oid = "PowerNet-MIB::upsAdvBatteryInternalSKU.0"; }
+                { name = "batteryExternalSku"; oid = "PowerNet-MIB::upsAdvBatteryExternalSKU.0"; }
                 { name = "batteryActualVoltage"; oid = "PowerNet-MIB::upsAdvBatteryActualVoltage.0"; }
                 { name = "batteryCapacity"; oid = "PowerNet-MIB::upsAdvBatteryCapacity.0"; }
                 { name = "batteryNumOfBattPacks"; oid = "PowerNet-MIB::upsAdvBatteryNumOfBattPacks.0"; }
                 { name = "batteryRunTimeRemaining"; oid = "PowerNet-MIB::upsAdvBatteryRunTimeRemaining.0"; }
                 { name = "batteryReplaceDate"; oid = "PowerNet-MIB::upsBasicBatteryLastReplaceDate.0"; }
                 { name = "batteryReplaceIndicator"; oid = "PowerNet-MIB::upsAdvBatteryReplaceIndicator.0"; }
+                { name = "batteryReplaceRecommendedDate"; oid = "PowerNet-MIB::upsAdvBatteryRecommendedReplaceDate.0"; }
                 { name = "batteryStatus"; oid = "PowerNet-MIB::upsBasicBatteryStatus.0"; }
                 { name = "batteryTemperature"; oid = "PowerNet-MIB::upsAdvBatteryTemperature.0"; }
                 { name = "batteryTimeON"; oid = "PowerNet-MIB::upsBasicBatteryTimeOnBattery.0"; }
                 { name = "configNumDevices"; oid = "PowerNet-MIB::upsBasicConfigNumDevices.0"; }
-                { name = "firmwareRevision"; oid = "PowerNet-MIB::upsAdvIdentFirmwareRevision.0"; }
-                { name = "host"; oid = "PowerNet-MIB::upsBasicIdentName.0"; is_tag = true; }
                 { name = "inputFrequency"; oid = "PowerNet-MIB::upsHighPrecInputFrequency.0"; conversion = "float(1)"; }
                 { name = "inputLineFailCause"; oid = "PowerNet-MIB::upsAdvInputLineFailCause.0"; }
                 { name = "inputLineVoltage"; oid = "PowerNet-MIB::upsHighPrecInputLineVoltage.0"; conversion = "float(1)"; }
                 { name = "inputLineVoltageMin"; oid = "PowerNet-MIB::upsHighPrecInputMinLineVoltage.0"; conversion = "float(1)"; }
                 { name = "inputLineVoltageMax"; oid = "PowerNet-MIB::upsHighPrecInputMaxLineVoltage.0"; conversion = "float(1)"; }
-                { name = "model"; oid = "PowerNet-MIB::upsBasicIdentModel.0"; }
                 { name = "outputStatus"; oid = "PowerNet-MIB::upsBasicOutputStatus.0"; }
-                { name = "outputCurrent"; oid = "PowerNet-MIB::upsHighPrecOutputLoad.0"; conversion = "float(1)"; }
+                { name = "outputActivePower"; oid = "PowerNet-MIB::upsAdvOutputActivePower.0"; }
+                { name = "outputApparentPower"; oid = "PowerNet-MIB::upsAdvOutputApparentPower.0"; }
+                { name = "outputCurrent"; oid = "PowerNet-MIB::upsHighPrecOutputCurrent.0"; conversion = "float(1)"; }
                 { name = "outputFrequency"; oid = "PowerNet-MIB::upsHighPrecOutputFrequency.0"; conversion = "float(1)"; }
                 { name = "outputLoad"; oid = "PowerNet-MIB::upsHighPrecOutputLoad.0"; conversion = "float(1)"; }
                 { name = "outputVoltage"; oid = "PowerNet-MIB::upsHighPrecOutputVoltage.0"; conversion = "float(1)"; }
-                { name = "serialNumber"; oid = "PowerNet-MIB::upsAdvIdentSerialNumber.0"; }
                 { name = "testCalibrationLastRunning"; oid = "PowerNet-MIB::upsAdvTestCalibrationDate.0"; }
                 { name = "testCalibrationLastSuccessful"; oid = "PowerNet-MIB::upsAdvTestCalibrationLastSuccessfulDate.0"; }
                 { name = "testCalibrationResults"; oid = "PowerNet-MIB::upsAdvTestCalibrationResults.0"; }
