d03c8ccd40112a248c8c43f9521ebc0a238dd0c8
senpro-ingwersenk <kevin.ingwersen@senpro.it>
2025-11-04 09:08:08 +0100

    Fix syntax

--- nixos/default.nix
+++ nixos/default.nix
@@ -7,15 +7,17 @@
     ./oci-containers
   ];
 
-  config.system.activationScripts = {
-    senpro_backupConfig = ''
-      echo "[TODO] Config backup goes here..."
-    '';
+  config = {
+    system.activationScripts = {
+      senpro_backupConfig = ''
+        echo "[TODO] Config backup goes here..."
+      '';
+    };
+
+    # Disable auto upgrades; we do tem ourself.
+    system.autoUpgrade.enable = false;
+
+    # Do not run this.
+    nix.gc.automatic = false;
   };
-
-  # Disable auto upgrades; we do tem ourself.
-  system.autoUpgrade.enable = false;
-
-  # Do not run this.
-  nix.gc.automatic = false;
 }
